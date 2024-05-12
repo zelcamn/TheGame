4fd5b2f4b17249091c49b4284c438965fcada278
zelcamn <zelcamn@yandex.ru>
2024-05-12 20:53:31 +0700

    #30 добавить обнаружение тайлсета, отредактировать ии

--- gameFiles/enemies/basic_enemy.gd
+++ gameFiles/enemies/basic_enemy.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
 var tween: Tween
+var params = PhysicsRayQueryParameters2D.new()
 
+@export var viewDistance = 10
 @export var patrolDistance = 70
 @export var speed: int = 1
 @export var waitTimer = 3
@@ -10,6 +12,7 @@ var startChanged = false
 var isIdle = true
 var isReachedDest = false
 var doAttack = false
+
 var destinationPoint: Vector2
 var player: Object
 var homePoint: Vector2
@@ -18,7 +21,9 @@ var moveDelta = 0.0
 var timer = 0.0
 
 func _ready():
-	destinationPoint = transform.origin + _random_inside_unit_circle()
+	params.exclude = [self]
+	$DetectCollisions/Detection.shape.set_radius(viewDistance)
+	destinationPoint = transform.origin
 	startPoint = transform.origin
 	player = get_tree().get_nodes_in_group("Player")[0]
 	homePoint = self.position
@@ -27,14 +32,23 @@ func _physics_process(delta):
 	if moveDelta >= 1:
 		timer += delta
 	if isIdle == false:
-		destinationPoint = get_pursue_point()
+		if isPlayerVisible():
+			startPoint = transform.origin
+			moveDelta = 0.0
+			destinationPoint = get_pursue_point()
+		else:
+			pass
+			#destinationPoint = get_patrol_point()
 	else:
-		destinationPoint = get_patrol_point()
+		pass
+		#destinationPoint = get_patrol_point()
+	#print(homePoint, destinationPoint, homePoint.distance_to(destinationPoint))
 	if moveDelta < 1 or isIdle == false:
 		travel(destinationPoint, delta)
 
 func get_patrol_point() -> Vector2:
-	var result = destinationPoint
+	var space = get_world_2d().direct_space_state
+	var destination = destinationPoint
 	if moveDelta >= 1 and timer >= waitTimer:
 		moveDelta = 0.0
 		timer = 0.0
@@ -42,9 +56,20 @@ func get_patrol_point() -> Vector2:
 		if startChanged:
 			homePoint = startPoint
 			startChanged = false
-		result = homePoint + _random_inside_unit_circle() * patrolDistance
-		
-	return result
+		var dirs = [0, 1, 2, 3, 4, 5, 6, 7]
+		for i in range(8):
+			destination = ((homePoint + Vector2(0, randf() * patrolDistance) )* Vector2.UP.rotated(dirs.pick_random() * 45)).clamp(homePoint - Vector2(patrolDistance, patrolDistance), homePoint + Vector2(patrolDistance, patrolDistance))
+			params.set_from(transform.origin)
+			params.set_to(destination)
+			print(space.intersect_ray(params))
+			if space.intersect_ray(params) == {}:
+				break
+	#params.set_from(transform.origin)
+	#params.set_to(destination)
+	#var result = space.intersect_ray(params)
+	#if result == null:
+	#	return destination
+	return destination
 
 func get_pursue_point() -> Vector2:
 	if moveDelta >= 1:
@@ -53,17 +78,27 @@ func get_pursue_point() -> Vector2:
 	startChanged = true
 	return player.position
 
+func isPlayerVisible():
+	var space = get_world_2d().direct_space_state
+	params.set_from(transform.origin)
+	params.set_to(player.position)
+	var result = space.intersect_ray(params)
+	if result.collider == player:
+		return true
+	return false
+	
+
 func travel(dest: Vector2, delta):
 	#var distance = transform.origin.distance_to(dest)
 	#var lerpTime = distance / speed
 	#moveDelta += delta / lerpTime
 	#translate(lerp(transform.origin, dest, moveDelta) - transform.origin)
-	
-	var distance = dest.distance_to(startPoint)
-	var lerpTime = distance / speed
-	moveDelta += delta / lerpTime
-	var nextPoint = startPoint.lerp(dest, moveDelta)
-	transform.origin = nextPoint
+	if transform.origin != dest:
+		var distance = dest.distance_to(startPoint)
+		var lerpTime = distance / speed
+		moveDelta += delta / lerpTime
+		var nextPoint = startPoint.lerp(dest, moveDelta)
+		transform.origin = nextPoint
 
 func attack(body: Object):
 	print("attack!!!!")
@@ -72,8 +107,6 @@ func attack(body: Object):
 func is_player_entered_detection(body):
 	print(player, "   ", body)
 	if body == player:
-		startPoint = transform.origin
-		moveDelta = 0.0
 		isIdle = false
 
 func is_player_exited_detection(body):
--- gameFiles/player/PlayerController.gd
+++ gameFiles/player/PlayerController.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var speed = GlobalInfo.playerSpeed
+@export var speed = 10
 var screenSize
 var health = Health.new()
 var inventory = Inventory.new()
@@ -20,7 +20,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	var velocity = Vector2.ZERO
+	velocity = Vector2.ZERO
 	if Input.is_action_pressed("moveDown"):
 		velocity.y += 1
 	if Input.is_action_pressed("moveUp"):
@@ -38,7 +38,8 @@ func _physics_process(delta):
 		$PlayerSprite.animation = "stop"
 		$PlayerSprite.stop()
 	
-	position += velocity * delta
+	move_and_slide()
+	#position += velocity * delta
 	#position = position.clamp(Vector2.ZERO, screenSize)
 
 func add_item(item: Resource, index: int):
